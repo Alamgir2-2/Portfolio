299387153dc291c8051de0206e4483f1bd389ebd
Alamgir2-2 <darkthehunter22@gmail.com>
2025-03-19 14:27:28 +0600

    Footer Updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,21 +1,22 @@
-import { FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa';
+import { FaFacebook, FaGithub, FaLinkedin } from 'react-icons/fa';
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-800 text-white py-8">
-      <div className="max-w-6xl mx-auto px-4 flex flex-col items-center">
+    <footer className=" text-white mt-10 py-8 px-18">
+      <hr />
+      <div className="max-w-6xl mx-auto px-4 p-8 flex flex-col items-center">
         <div className="flex space-x-6 mb-4">
-          <a href="https://github.com" className="hover:text-blue-400 transition duration-300">
+          <a href="https://github.com/Alamgir2-2" className="hover:text-blue-400 transform transition duration-300">
             <FaGithub size={24} />
           </a>
-          <a href="https://linkedin.com" className="hover:text-blue-400 transition duration-300">
+          <a href="https://www.linkedin.com/in/alamgir22/" className="hover:text-blue-400 transform transition duration-300">
             <FaLinkedin size={24} />
           </a>
-          <a href="https://twitter.com" className="hover:text-blue-400 transition duration-300">
-            <FaTwitter size={24} />
+          <a href="https://www.facebook.com/Alamgir.2.web/" className="hover:text-blue-400 transform transition duration-300">
+            <FaFacebook size={24} />
           </a>
         </div>
-        <p className="text-sm">&copy; 2023 Your Name. All rights reserved.</p>
+        <p className="text-sm">&copy; 2025, Alamgir. All rights reserved.</p>
       </div>
     </footer>
   );
